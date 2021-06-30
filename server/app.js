c0641898b2187ac77ff47f51f552fbe122ce37ab
Zachary Sawyer <mountainsounds15@gmail.com>
2021-06-30 15:37:54 -0400

    Finished API routing, incorporated indexing, refactored joins. Improved query speed from 10-30seconds to 5-30ms;

--- server/app.js
+++ server/app.js
@@ -6,23 +6,34 @@ const helpers = require('./helpers.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// SELECT * FROM B right join On e
+
+// SELECT * FROM reviews
+// SELECT employees.first_name, employees.last_name, salaries.salary FROM employees LEFT JOIN salaries ON employees.emp_no = salaries.emp_no WHERE employees.emp_no = 10001;
+
+// GROUP BY employees.emp_no;
+// array_agg()
+// employees.*
+
+
 app.get('/reviews', async (req, res) => {
   let params = helpers.parseQuery({page: 1, count: 5, product_id: 1}, req._parsedUrl.query);
 
-  // let joinQuery = 'INNER JOIN reviews_photos ON reviews.id = reviews_photos.review_id';
-  let selectQuery = `SELECT * FROM reviews WHERE product_id = ?`;
+  let selectQuery = `SELECT reviews.*, GROUP_CONCAT(reviews_photos.url) AS photos FROM reviews
+     LEFT JOIN reviews_photos
+        ON reviews.id = reviews_photos.review_id
+     WHERE product_id = ?
+     GROUP BY reviews.id`;
 
   let reviews = await db.promise().query(selectQuery, [params.product_id]);
-
   for (let review of reviews[0]) {
-    let photos = await db.promise().query('SELECT url FROM reviews_photos WHERE review_id = ?', [review.id]);
-    review.photos = photos[0].map(photo => photo.url);
+    review.photos = review.photos === null ? [] : review.photos.split(',');
   }
 
   res.send(reviews[0]);
 });
 
-app.get('/reviews/meta', async (req, res) => {
+app.get('/reviews/meta', async (req, res) => {``
   let params = helpers.parseQuery({product_id: 1}, req._parsedUrl.query);
   let result = {
     product_id: params.product_id,
@@ -38,25 +49,21 @@ app.get('/reviews/meta', async (req, res) => {
   }
 
   let reviews = await db.promise().query(`SELECT * FROM reviews WHERE product_id = ${params.product_id}`);
-  console.log('reviews: ', reviews[0]);
   let characteristics = await db.promise().query(`SELECT * FROM characteristics WHERE product_id = ${params.product_id}`);
 
-  for (let review of reviews[0]) {
-    if (review.recommend === 'true') result.recommended['0']++;
-    result.ratings[review.rating] += 1;
+  for (let {recommend, rating} of reviews[0]) {
+    if (recommend === 'true') result.recommended['0']++;
+    result.ratings[rating] += 1;
   }
 
   for (let char of characteristics[0]) {
     let char_reviews = await db.promise().query(`SELECT * FROM characteristic_reviews WHERE characteristic_id = ${char.id}`);
     let val = 0;
-    console.log('Char_reviews: ', char_reviews);
     if (char_reviews[0].length === 0) break;
 
-    for (let {value} of char_reviews[0]) {
-      console.log('val: ', val);
-      val += value
-    }
+    for (let {value} of char_reviews[0]) val += value;
     val /= char_reviews[0].length;
+
     result.characterisitics[char.name] = {
       id: char.id,
       value: String(val),
@@ -73,26 +80,30 @@ app.post('/reviews', async (req, res) => {
 
     let insertReviewQ = `INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) VALUES (${product_id}, ${rating}, ${today}, '${summary}', '${body}', '${recommend}','${review.reported || false}', '${name}', '${email}', ${review.response || null}, ${review.helpfulness || 0});`;
 
-    let responsePostInsert = await db.promise().query(insertReviewQ);
+    await db.promise().query(insertReviewQ);
 
     let reviewId = await db.promise().query(`SELECT max(id) FROM reviews;`);
     reviewId = reviewId[0][0]['max(id)']
     let insertPhotoQ = `INSERT INTO reviews_photos (review_id, url) VALUES (${reviewId}, '${review.photos}')`
-    let responsePostPhotoInsert = await db.promise().query(insertPhotoQ);
-
+    await db.promise().query(insertPhotoQ);
 
     res.status(200).send('Adding a review was a success!')
 });
 
+app.put(`/reviews/:review_id/:type`, async (req, res) => {
+  const {review_id, type} = req.params;
+  let review = await db.promise().query('SELECT reported, helpfulness FROM reviews WHERE id = ?', [review_id]);
+  let {reported, helpfulness} = review[0][0];
 
+  if (type === 'report') {
+      reported = reported === 'true' ? 'false' : 'true';
+      db.promise().query(`UPDATE reviews SET reported = '${reported}' WHERE id = ?`, [review_id]);
+  } else if (type === 'helpful') {
+      helpfulness += 1;
+      db.promise().query(`UPDATE reviews SET helpfulness = ${helpfulness} WHERE id = ?`, [review_id]);
+  }
 
-
-app.put('/reviews/:review_id/helpful', async (req, res) => {
-
-});
-
-app.put('/reviews/:review_id/report', async (req, res) => {
-
+  res.status(200).send('Updating a review was a success!');
 });
 
 
